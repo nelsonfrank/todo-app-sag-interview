3e3bae52c675b06156c107f1556a0973af990792
NelsonFrank <nelsonfr78@outlook.com>
2024-03-25 19:26:36 +0300

    feat: :sparkles: integrate dashboard with data

--- src/app/analytics/overview.tsx
+++ src/app/analytics/overview.tsx
@@ -1,59 +1,64 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 "use client";
 
 import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
 
-const data = [
-  {
-    name: "9am",
-    total: Math.floor(Math.random() * 10),
-  },
-  {
-    name: "10am",
-    total: Math.floor(Math.random() * 10),
-  },
-  {
-    name: "11am",
-    total: Math.floor(Math.random() * 10),
-  },
-  {
-    name: "12pm",
-    total: Math.floor(Math.random() * 10),
-  },
-  {
-    name: "1pm",
-    total: Math.floor(Math.random() * 10),
-  },
-  {
-    name: "2pm",
-    total: Math.floor(Math.random() * 10),
-  },
-  {
-    name: "3pm",
-    total: Math.floor(Math.random() * 10),
-  },
-  {
-    name: "4pm",
-    total: Math.floor(Math.random() * 10),
-  },
-  {
-    name: "5pm",
-    total: Math.floor(Math.random() * 10),
-  },
-  {
-    name: "6pm",
-    total: Math.floor(Math.random() * 10),
-  },
-  {
-    name: "7pm",
-    total: Math.floor(Math.random() * 10),
-  },
-  {
-    name: "8pm",
-    total: Math.floor(Math.random() * 10),
-  },
-];
+// const data = [
+//   {
+//     name: "9am",
+//     total: Math.floor(Math.random() * 10),
+//   },
+//   {
+//     name: "10am",
+//     total: Math.floor(Math.random() * 10),
+//   },
+//   {
+//     name: "11am",
+//     total: Math.floor(Math.random() * 10),
+//   },
+//   {
+//     name: "12pm",
+//     total: Math.floor(Math.random() * 10),
+//   },
+//   {
+//     name: "1pm",
+//     total: Math.floor(Math.random() * 10),
+//   },
+//   {
+//     name: "2pm",
+//     total: Math.floor(Math.random() * 10),
+//   },
+//   {
+//     name: "3pm",
+//     total: Math.floor(Math.random() * 10),
+//   },
+//   {
+//     name: "4pm",
+//     total: Math.floor(Math.random() * 10),
+//   },
+//   {
+//     name: "5pm",
+//     total: Math.floor(Math.random() * 10),
+//   },
+//   {
+//     name: "6pm",
+//     total: Math.floor(Math.random() * 10),
+//   },
+//   {
+//     name: "7pm",
+//     total: Math.floor(Math.random() * 10),
+//   },
+//   {
+//     name: "8pm",
+//     total: Math.floor(Math.random() * 10),
+//   },
+// ];
 
-export function Overview() {
+export function Overview({
+  data,
+}: {
+  data: { name: string; total: number }[];
+}) {
   return (
     <ResponsiveContainer width="100%" height={350}>
       <BarChart data={data}>
--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,17 +1,9 @@
-import Link from "next/link";
-import {
-  Activity,
-  ArrowUpRight,
-  CircleUser,
-  CreditCard,
-  DollarSign,
-  Menu,
-  Package2,
-  Search,
-  Users,
-} from "lucide-react";
-import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
-import { Button } from "~/components/ui/button";
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { CreditCard, DollarSign, Users } from "lucide-react";
 import {
   Card,
   CardContent,
@@ -20,184 +12,153 @@ import {
   CardTitle,
 } from "~/components/ui/card";
 import { Overview } from "./overview";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
+import { api } from "~/trpc/server";
+import { TodoItem } from "~/components/todo-item";
+import { add, compareAsc, endOfWeek, format, startOfWeek, sub } from "date-fns";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const todos = await api.todo.getAll();
+
+  const totalCompletedTodos = todos.filter((todo) => todo.isComplete);
+  const totalPendingTodos = todos.filter((todo) => !todo.isComplete);
+
+  const currentDate = new Date();
+  const startDate = startOfWeek(currentDate, { weekStartsOn: 0 }); // Week starts on Monday (0 for Sunday, 1 for Monday, etc.)
+  const endDate = endOfWeek(currentDate, { weekStartsOn: 0 });
+
+  const generateGraphData = (todoItems: typeof todos) => {
+    const names = [
+      { name: "Sunday", total: 0 },
+      { name: "Monday", total: 0 },
+      { name: "Tuesday", total: 0 },
+      { name: "Wednesday", total: 0 },
+      { name: "Thursday", total: 0 },
+      { name: "Friday", total: 0 },
+      { name: "Saturday", total: 0 },
+    ];
+
+    const data = todoItems
+      .filter(
+        (todo) =>
+          compareAsc(
+            new Date(todo.dueDate).getDate(),
+            new Date(startDate).getDate(),
+          ) >= 1 &&
+          compareAsc(
+            new Date(todo.dueDate).getDate(),
+            new Date(add(endDate, { days: 1 })).getDate(),
+          ) <= -1,
+      )
+      .reduce((acc, obj) => {
+        // Extract day from the object's date property
+        const day = format(new Date(obj.dueDate).toDateString(), "EEEE");
+
+        // Check if the day already exists in the accumulator object
+        if (!acc[day]) {
+          // If not, create a new array with the object
+          acc[day] = [obj];
+        } else {
+          // If exists, push the object to the existing array
+          acc[day].push(obj);
+        }
+
+        return acc;
+      }, {});
+
+    return names.map((item) => {
+      if (data[item.name]) {
+        item.total = data[item.name].length;
+      }
+
+      return item;
+    });
+  };
+
+  const weeklyCompletedTodosStats = generateGraphData(totalCompletedTodos);
   return (
     <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
       <div className="flex items-center">
         <h1 className="text-lg font-semibold md:text-2xl">Analytics</h1>
       </div>
       <div className="flex min-h-screen w-full flex-col">
-        <Tabs defaultValue="overview" className="space-y-4">
-          <TabsList>
-            <TabsTrigger value="overview">Today</TabsTrigger>
-            <TabsTrigger value="analytics" disabled>
-              This Week
-            </TabsTrigger>
-            <TabsTrigger value="reports" disabled>
-              This Month
-            </TabsTrigger>
-            <TabsTrigger value="notifications" disabled>
-              All Time
-            </TabsTrigger>
-          </TabsList>
-          <TabsContent value="overview" className="space-y-4">
-            <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
-              <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
-                <Card>
-                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">
-                      Total Revenue
-                    </CardTitle>
-                    <DollarSign className="h-4 w-4 text-muted-foreground" />
-                  </CardHeader>
-                  <CardContent>
-                    <div className="text-2xl font-bold">$45,231.89</div>
-                    <p className="text-xs text-muted-foreground">
-                      +20.1% from last month
-                    </p>
-                  </CardContent>
-                </Card>
-                <Card>
-                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">
-                      Subscriptions
-                    </CardTitle>
-                    <Users className="h-4 w-4 text-muted-foreground" />
-                  </CardHeader>
-                  <CardContent>
-                    <div className="text-2xl font-bold">+2350</div>
-                    <p className="text-xs text-muted-foreground">
-                      +180.1% from last month
-                    </p>
-                  </CardContent>
-                </Card>
-                <Card>
-                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">Sales</CardTitle>
-                    <CreditCard className="h-4 w-4 text-muted-foreground" />
-                  </CardHeader>
-                  <CardContent>
-                    <div className="text-2xl font-bold">+12,234</div>
-                    <p className="text-xs text-muted-foreground">
-                      +19% from last month
-                    </p>
-                  </CardContent>
-                </Card>
-                <Card>
-                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">
-                      Active Now
-                    </CardTitle>
-                    <Activity className="h-4 w-4 text-muted-foreground" />
-                  </CardHeader>
-                  <CardContent>
-                    <div className="text-2xl font-bold">+573</div>
-                    <p className="text-xs text-muted-foreground">
-                      +201 since last hour
-                    </p>
-                  </CardContent>
-                </Card>
-              </div>
-              <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
-                <Card className="xl:col-span-2">
-                  <CardHeader className="flex flex-row items-center">
-                    <div className="grid gap-2">
-                      <CardTitle>Overview</CardTitle>
-                      <CardDescription>Recent tasks overview</CardDescription>
-                    </div>
-                  </CardHeader>
-                  <CardContent>
-                    <Overview />
-                  </CardContent>
-                </Card>
-                <Card>
-                  <CardHeader>
-                    <CardTitle>Recent Sales</CardTitle>
-                  </CardHeader>
-                  <CardContent className="grid gap-8">
-                    <div className="flex items-center gap-4">
-                      <Avatar className="hidden h-9 w-9 sm:flex">
-                        <AvatarImage src="/avatars/01.png" alt="Avatar" />
-                        <AvatarFallback>OM</AvatarFallback>
-                      </Avatar>
-                      <div className="grid gap-1">
-                        <p className="text-sm font-medium leading-none">
-                          Olivia Martin
-                        </p>
-                        <p className="text-sm text-muted-foreground">
-                          olivia.martin@email.com
-                        </p>
-                      </div>
-                      <div className="ml-auto font-medium">+$1,999.00</div>
-                    </div>
-                    <div className="flex items-center gap-4">
-                      <Avatar className="hidden h-9 w-9 sm:flex">
-                        <AvatarImage src="/avatars/02.png" alt="Avatar" />
-                        <AvatarFallback>JL</AvatarFallback>
-                      </Avatar>
-                      <div className="grid gap-1">
-                        <p className="text-sm font-medium leading-none">
-                          Jackson Lee
-                        </p>
-                        <p className="text-sm text-muted-foreground">
-                          jackson.lee@email.com
-                        </p>
-                      </div>
-                      <div className="ml-auto font-medium">+$39.00</div>
-                    </div>
-                    <div className="flex items-center gap-4">
-                      <Avatar className="hidden h-9 w-9 sm:flex">
-                        <AvatarImage src="/avatars/03.png" alt="Avatar" />
-                        <AvatarFallback>IN</AvatarFallback>
-                      </Avatar>
-                      <div className="grid gap-1">
-                        <p className="text-sm font-medium leading-none">
-                          Isabella Nguyen
-                        </p>
-                        <p className="text-sm text-muted-foreground">
-                          isabella.nguyen@email.com
-                        </p>
-                      </div>
-                      <div className="ml-auto font-medium">+$299.00</div>
-                    </div>
-                    <div className="flex items-center gap-4">
-                      <Avatar className="hidden h-9 w-9 sm:flex">
-                        <AvatarImage src="/avatars/04.png" alt="Avatar" />
-                        <AvatarFallback>WK</AvatarFallback>
-                      </Avatar>
-                      <div className="grid gap-1">
-                        <p className="text-sm font-medium leading-none">
-                          William Kim
-                        </p>
-                        <p className="text-sm text-muted-foreground">
-                          will@email.com
-                        </p>
-                      </div>
-                      <div className="ml-auto font-medium">+$99.00</div>
-                    </div>
-                    <div className="flex items-center gap-4">
-                      <Avatar className="hidden h-9 w-9 sm:flex">
-                        <AvatarImage src="/avatars/05.png" alt="Avatar" />
-                        <AvatarFallback>SD</AvatarFallback>
-                      </Avatar>
-                      <div className="grid gap-1">
-                        <p className="text-sm font-medium leading-none">
-                          Sofia Davis
-                        </p>
-                        <p className="text-sm text-muted-foreground">
-                          sofia.davis@email.com
-                        </p>
-                      </div>
-                      <div className="ml-auto font-medium">+$39.00</div>
-                    </div>
-                  </CardContent>
-                </Card>
-              </div>
-            </main>
-          </TabsContent>
-        </Tabs>
+        <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+          <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
+            <Card>
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-medium">
+                  Total Completed Todos
+                </CardTitle>
+                <DollarSign className="h-4 w-4 text-muted-foreground" />
+              </CardHeader>
+              <CardContent>
+                <div className="text-2xl font-bold">
+                  {totalCompletedTodos.length}
+                </div>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-medium">
+                  Total Pending Todos
+                </CardTitle>
+                <Users className="h-4 w-4 text-muted-foreground" />
+              </CardHeader>
+              <CardContent>
+                <div className="text-2xl font-bold">
+                  {totalPendingTodos.length}
+                </div>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-medium">
+                  Total Todos
+                </CardTitle>
+                <CreditCard className="h-4 w-4 text-muted-foreground" />
+              </CardHeader>
+              <CardContent>
+                <div className="text-2xl font-bold">{todos.length}</div>
+              </CardContent>
+            </Card>
+          </div>
+          <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
+            <Card className="xl:col-span-2">
+              <CardHeader className="flex flex-row items-center">
+                <div className="grid gap-2">
+                  <CardTitle>Weekly Overview</CardTitle>
+                  <CardDescription>
+                    This week completed todos - {format(startDate, "PPPP")} -{" "}
+                    {format(endDate, "PP")}
+                  </CardDescription>
+                </div>
+              </CardHeader>
+              <CardContent>
+                <Overview data={weeklyCompletedTodosStats} />
+              </CardContent>
+            </Card>
+            <Card>
+              <CardHeader>
+                <CardTitle>Recent Todos</CardTitle>
+              </CardHeader>
+              <CardContent className="grid gap-8">
+                <div className="mt-4">
+                  {todos.map(({ todo, dueDate, priority, id, isComplete }) => (
+                    <TodoItem
+                      todoItem={{
+                        todo,
+                        dueDate,
+                        priority,
+                        id,
+                        isComplete,
+                      }}
+                      key={id}
+                    />
+                  ))}
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+        </main>
       </div>
     </main>
   );
